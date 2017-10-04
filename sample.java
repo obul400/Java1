3afd3693c72a1e282f6c5525b35b59d5fd86eca0
obul1122 <obul400@gmail.com>
2017-10-04 14:32:24 -0500

    added

--- sample.java
+++ sample.java
@@ -5,6 +5,9 @@ class Sample
 public static void main(String[] args)
 {
    for(int i=0;i<=10;i++)
+{
    System.out.println("print something to show programme"+i);
+   System.out.println("Modified from github by obul"+i);
+}
 }
 }
